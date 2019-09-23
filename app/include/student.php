702701907f19f584e1fb154ad8291fa4c67da8ba
Hui Sin <huisin.lim.2018@smu.edu.sg>
2019-09-23 16:25:16 +0800

    creating bootstrap and coding student class

---
+++ app/include/student.php
@@ -0,0 +1,39 @@
+<?php
+
+class Student{
+    //property declaration
+    public $userid;
+    public $password;
+    public $name;
+    public $school;
+    public $edollar;
+
+    public function __construct($userid, $password, $name, $school, $edollar){
+        $this->userid = $userid;
+        $this->password = $password;
+        $this->name = $name;
+        $this->school = $school;
+        $this->edollar = $edollar; 
+    }
+
+    public function getUserid(){
+        return $this->userid;
+    }
+
+    public function getPwd(){
+        return $this->password;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getSchool(){
+        return $this->school;
+    }
+
+    public function getEdollar(){
+        return $this->edollar;
+    }
+}
+?>
