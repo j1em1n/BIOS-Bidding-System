38aaab84725ca7be923a5e0a72f26dc1c83f485e
Hui Sin <huisin.lim.2018@smu.edu.sg>
2019-09-30 16:14:44 +0800

    updating bootstrap to include validation for student
    Co-Authored-By: j1em1n <j1em1n@users.noreply.github.com>

--- app/include/bootstrap.php
+++ app/include/bootstrap.php
@@ -80,26 +80,109 @@ function doBootstrap() {
 				# start processing
 				# truncate current SQL tables
 				
-				$studentDAOobj = new UserDAO();
-				$studentDAOobj->removeAll(); #clearing data (not database)
+				$studentDAOobj = new StudentDAO();
+                $studentDAOobj->removeAll();
+
+                $courseDAOobj = new CourseDAO();
+                $courseDAOobj->removeAll();
+
+                $sectionDAOobj = new SectionDAO();
+                $sectionDAOobj->removeAll();
+                
+                $prerequisiteDAOobj = new PrerequisiteDAO();
+                $prerequisiteDAOobj->removeAll();
+                
+                $courseCompletedDAOobj = new CoursecompletedDAO();
+                $courseCompletedDAOobj->removeAll();
+
+                $bidDAOobj = new BidDAO();
+                $bidDAOobj->removeAll();
 
 				# then read each csv file line by line (remember to skip the header)
 				# $data = fgetcsv($file) gets you the next line of the CSV file which will be stored 
 				# in the array $data
 				# $data[0] is the first element in the csv row, $data[1] is the 2nd, ....
 				
+				// STUDENT 
+
 				#skip header
-				$data = fgetcsv($student); #will get array  in data (2 fields cause csv files only have 2 columns)
-				#give a file to read i.e. $pokemon 
+				$data = fgetcsv($student); #will get array in data (2 fields cause csv files only have 2 columns)
+				#give a file to read  
 				while ( ($data = fgetcsv($student) ) !== false){ #double == to check for boolean also. 
-					# $data[0] = name $data[1] = type
-					$studentObj = new Student ($data[0], $data[2], $data[3], $data[4]);
-					$studentDAOobj->add($studentObj);
-					$pokemon_processed++;
+					$countStud = 1;
+					//Trim all the variables to ensure that there's no whitespace from both sides of the string using trim()
+					$userId = trim($data[0]);
+					$pwd = trim($data[1]);
+					$name = trim($data[2]);
+					$school = trim($data[3]);
+					$edollar = trim($data[4]);
+					//Check for any field 
+					if(!(empty($userId) || empty($pwd) || empty($name) || empty($school) || empty($edollar))){
+						//An indexed array to store all the exisiting userid in, to check for duplicate userid
+						$checkDupUserId[] = $userId;
+						//Checking if the userid field is > 128 characters using strlen()
+						if(strlen($userId) <= 128){
+							//Checking for any duplicate userId
+							if(in_array($userId, $checkDupUserId)){
+								$_SESSION['errors'] = "student.csv - row $countStud - there is an existing user with the same userid.";
+							}
+						} else {
+							$_SESSION['errors'] = "student.csv - row $countStud - the userid field must not exceed 128 characters.";
+						}
+						if($edollar >= 0.0){
+							//Check whether the double has more than 2 decimal place 
+							$checkedollar = strval($edollar);
+							$edollarArr = explode(".", $checkedollar);
+							if(strlen($edollarArr[1]) > 2){
+								$_SESSION['errors'] = "student.csv - row $countStud - edollar shouldn't have more than 2 decimal place.";
+							} 
+						} else {
+							$_SESSION['errors'] = "student.csv - row $countStud - edollar should be more than or equals to 0.0.";
+						}
+						//Checking if the password field has > 128 characters using strlen()
+						if(strlen($pwd) > 128){
+							$_SESSION['errors'] = "student.csv - row $countStud - password should not contain more than 128 characters.";
+						}
+						//Checking if the name field has > 100 characters using strlen()
+						if(strlen($name) > 100){
+							$_SESSION['errors'] = "student.csv - row $countStud - name should not contain more than 100 characters.";
+						}
+						if(count($_SESSION['errors']) == 0){
+							//Convert edollar to string before storing it into database as pdo dun have double. :/ need to change database? 
+							$edollar = strval($edollar);
+							$studentObj = new Student($userId, $pwd, $name, $school, $edollar);
+							$studentDAOobj->add($studentObj);
+							$student_processed++; #line added successfully	
+						} else {
+							//Print out all the errors for user
+							//print error for blank fields? CHECK!
+							printErrors();
+						}
+					} else {
+						//pass the line in the file (apparently dun need to write any code?? try try)
+						//Print out all the errors for user
+						if(empty($data[0])){
+							$_SESSION['errors'] = "userid field is blank.";
+						} 
+						if(empty($data[1])){
+							$_SESSION['errors'] = "password field is blank.";
+						}
+						if(empty($data[2])){
+							$_SESSION['errors'] = "name field is blank.";
+						}
+						if(empty($data[3])){
+							$_SESSION['errors'] = "school field is blank.";
+						}
+						if(empty($data[4])){
+							$_SESSION['errors'] = "edollar field is blank.";
+						}
+						printErrors();
+					}
+					$countStud++;
 				}
 
-				fclose($pokemon); //close file handle
-				unlink($pokemon_path); //delete the temp file
+				fclose($student); // close the file handle
+				unlink($student_path); // delete the temp file
 
 				# process each line and check for errors
 				# for this lab, assume the only error you should check for is that each CSV field 
