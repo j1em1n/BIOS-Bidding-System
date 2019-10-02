1ed159edeb8f8d559eec59c0e7dba23381829284
Lim Jie Min <jiemin.lim.2018@sis.smu.edu.sg>
2019-10-02 20:18:03 +0800

    add DAO and class for admin
    Co-Authored-By: huisinnnn <huisinnnn@users.noreply.github.com>

---
+++ app/include/admin.php
@@ -0,0 +1,25 @@
+<?php
+
+class Admin{
+    //property declaration
+    public $userid;
+    public $hashed_password;
+
+    public function __construct($userid, $hashed_password){
+        $this->userid = $userid;
+        $this->hashedpassword = $hashed_password;
+    }
+
+    public function getUserid(){
+        return $this->userid;
+    }
+
+    public function getPwd(){
+        return $this->hashed_password;
+    }
+
+    // public function authenticate($enteredPwd) {
+    //     return $this->hashed_password == $enteredPwd;
+    // }
+}
+?>
---
+++ app/include/adminDAO.php
@@ -0,0 +1,115 @@
+<?php
+
+spl_autoload_register(function($class){
+    require_once $class . ".php";
+});
+
+class AdminDAO {
+
+    public  function retrieveAll() {
+        $sql = 'SELECT * FROM admin ORDER BY userid';
+        
+            
+        $connMgr = new ConnectionManager();      
+        $conn = $connMgr->getConnection();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+        $result = array();
+
+        while($row = $stmt->fetch()) {
+            $result[] = new Admin($row['userid'], $row['password']);
+        }
+
+        $stmt = null;
+        $conn = null;
+
+        return $result;
+    }
+
+    function getHashedPassword($username){
+        $conn_manager = new ConnectionManager();
+        $pdo = $conn_manager->getConnection();
+        
+        $sql = "select * from admin where userid = :userid";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(":userid", $username);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        
+        $stmt->execute();
+        if($row = $stmt->fetch()){
+            
+           $hashed_password = $row["password"];
+
+        } else {
+            $hashed_password = FALSE;
+        }
+
+        $stmt->closeCursor();
+        $pdo = null;
+
+        return $hashed_password;
+    }
+
+    public  function retrieve($userid) {
+        $sql = 'SELECT * FROM admin ORDER BY userid';
+        
+            
+        $connMgr = new ConnectionManager();      
+        $conn = $connMgr->getConnection();
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+        $result = array();
+
+        while($row = $stmt->fetch()) {
+            $result[] = new Admin($row['userid'], $row['password']);
+        }
+
+        $stmt = null;
+        $conn = null;
+        
+        return $result;
+    }
+    
+    public function removeAll() {
+        $sql = 'TRUNCATE TABLE admin';
+        
+        $connMgr = new ConnectionManager();
+        $conn = $connMgr->getConnection();
+        
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->execute();
+        $count = $stmt->rowCount();
+    }  
+    
+    public function add($admin) {
+        $sql = 'INSERT INTO admin (userid, password) VALUES (:userid, :password)';
+        
+        $connMgr = new ConnectionManager();       
+        $conn = $connMgr->getConnection();
+         
+        $stmt = $conn->prepare($sql); 
+
+        $stmt->bindParam(':userid', $admin->getUserid(), PDO::PARAM_STR);
+        $stmt->bindParam(':password', $admin->getPwd(), PDO::PARAM_STR);
+
+
+        $isAddOK = False;
+        if ($stmt->execute()) {
+            $isAddOK = True;
+        }
+        
+        $stmt = null;
+        $conn = null;
+
+        return $isAddOK;
+    }
+    
+}
+?>
