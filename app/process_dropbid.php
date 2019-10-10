03fe487a91ef2a51858186147dc08914da7d97f2
hilyamy2018 <54932191+hilyamy2018@users.noreply.github.com>
2019-10-10 12:43:50 +0800

    Update process_dropbid.php
    Co-Authored-By: j1em1n <j1em1n@users.noreply.github.com>

--- app/process_dropbid.php
+++ app/process_dropbid.php
@@ -7,24 +7,61 @@
     $userid = $_SESSION['userid'];
 
 
+    //if everything is added in successfully
+    
     if (!empty($coursecode) && !empty($sectionnum)){
 
         $courseDAO = new CourseDAO();
         $sectionDAO = new SectionDAO();
         $bidDAO = new BidDAO();
+        $studentDAO = new StudentDAO();
 
         // Check if course exists in database
         if(!($courseDAO->retrieve($coursecode))) {
             $_SESSION['errors'][] = "invalid course code";
+
+            header("Location: dropbid.php");
+            exit();
+
         } else {
-            if (!($sectionDAO->retrieve($coursecode, $sectionnum))) {
+            if (!($sectionDAO->getSectionsByCourse($coursecode))) {
                 // Check if section code is found in section.csv (only for valid course code)
                 $_SESSION['errors'][] = "invalid section";
+
+                header("Location: dropbid.php");
+                exit();
+
+            } else {
+
+                $student = $studentDAO->retrieve($userid);
+                $currentedollars = $student->edollar;
+                $updatedamount = 0.0;
+                // get all bids from this user 
+                $listofbids = $bidDAO->retrieveByUserid($userid);
+                //var_dump($listofbids);
+                var_dump($currentedollars);
+
+                //specifiy which bid want to drop based on userid and coursecode and sectionnum
+                //foreach($listofbids as $eachbid){
+                    if($listofbids->userid == $userid && $listofbids->code == $coursecode && $listofbids->section == $sectionnum){
+                        // update edollars by subtraction
+                        $biddedamount = $listofbids->amount;                        
+                        $updatedamount = $currentedollars - $biddedamount;
+                        var_dump($updatedamount);
+                        var_dump($userid);
+
+                        // update in database
+                        $studentDAO->updateEdollar($userid, $updatedamount) ;
+                        
+                        header("Location: dropbid.php");
+                        exit();
+                    }
+
+                
+
+               
             } 
         }
-        header("Location: dropbid.php");
-        exit();
-            
         
     } else {
         if(empty($coursecode)) {
