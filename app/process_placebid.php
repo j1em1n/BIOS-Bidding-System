70588aa6181ffcb0f19e666bc60dc0dba84a9e99
Steffi <steffi.tan.2018@economics.smu.edu.sg>
2019-10-11 12:38:49 +0800

    Completed input data validations for process_placebid.php
    Co-Authored-By: slku1 <slku1@users.noreply.github.com>

--- app/process_placebid.php
+++ app/process_placebid.php
@@ -1,102 +1,113 @@
 <?php
 
-    $coursecode = $_POST['coursecode'];
-    $sectionnum = $_POST['sectionnum'];
+    $courseCode = $_POST['coursecode'];
+    $sectionNum = $_POST['sectionnum'];
     $edollar = $_POST['edollar'];
     $userid = $_SESSION['userid'];
 
-    if (!(empty($coursecode) || empty($sectionnnum) || empty($edollar))){
-
-        $courseDAO = new CourseDAO();
-        $sectionDAO = new SectionDAO();
-        $prerequisiteDAO = new PrerequisiteDAO();
-        $studentDAO = new StudentDAO();
-        $bidDAO = new BidDAO();
-        $roundDAO = new RoundDAO();
-        $roundObj = $roundDAO->retrieve();
-        $currentRound = $roundObj->getRound();
-        $student = $studentDAO->retrieve($userid);
-
-        // Check if edollar is a numeric value
-        if (!((is_numeric($edollar) || is_float($edollar)))){
-            $_SESSION['errors'][] = "invalid edollar";
-        } else {
-            // Check that edollar has at most 2dp
-            $checkededollar = strval($edollar);
-            $edollarArr = explode(".", $checkededollar);
-            if(strlen($edollarArr[1]) > 2){
-                $_SESSION['errors'][] = "edollar should have maximum 2 decimal places";
-            } elseif ($edollar > $student->getEdollar()) { // Check if student has enough e-dollars
-                $_SESSION['errors'][] = "you do not have enough e-dollars for this bid";
-            } elseif ( $edollar < 10.0) { // Check if edollar is greater than minimum value
-                $_SESSION['errors'][] = "your bid is less than the minimum bid";
-            }
-        }
-
-
-        // Check if course exists in database
-        if(!($courseDAO->retrieve($coursecode))) {
-            $_SESSION['errors'][] = "invalid course code";
-        } else {
-            if (!($sectionDAO->retrieve($coursecode, $sectionnum))) {
-                // Check if section code is found in section.csv (only for valid course code)
-                $_SESSION['errors'][] = "invalid section";
-            } else {
-                $studentBids = $bidDAO->retrieveByUserid($userid);
-                if(count($studentBids) == 5) {
-                    $_SESSION['errors'][] = "you already reached the maximum number of bids";
-                } else {
-                    $alreadyBidded = FALSE;
-                    foreach($studentBids as $b) {
-                        if ($b->getCode() == $coursecode) {
-                            $alreadyBidded == TRUE;
-                        }
-                    }
-                    if ($alreadyBidded) {
-                        $_SESSION['errors'][] = "you have already bidded for another section for this course";
-                    }
-                }
-            }
-        }
-        
-        
-        
-        
-        //Check whether current round is 1 or 2
-        elseif ($currentRound == 1) {
-            // if round == 1, student can only bid for courses from their school
-            $courseObj = $courseDAO->retrieve($coursecode);
-            if ($student->getSchool() != $courseObj->getSchool()){
-                $_SESSION['errors'][] = "please choose a course from your own school";
-            } elseif ($prerequisiteDAO->retrieve($coursecode)) { // Check if the course has a prerequisite
-                $prereqcourse = $prerequisiteDAO->retrieve($coursecode);
-                // Check if the student has completed the prerequisite (row with userid and prerequisite code exists)
-                if(!($courseCompletedDAO->retrieveByUserIdAndCode($userid, $prereqcourse->getPrerequisite()))){
-                    $_SESSION['errors'][] = "you have not completed the prerequisite course";
-                }
-            }
-        } elseif ($prerequisiteDAO->retrieve($coursecode)) { // Check if the course has a prerequisite
-            $prereqcourse = $prerequisiteDAO->retrieve($coursecode);
-            // Check if the student has completed the prerequisite (row with userid and prerequisite code exists)
-            if(!($courseCompletedDAO->retrieveByUserIdAndCode($userid, $prereqcourse->getPrerequisite()))){
-                $_SESSION['errors'][] = "you have not completed the prerequisite course";
-            }
-        }
-        
-        if()
-        
-
-    } else {
-        if(empty($coursecode)) {
-            $_SESSION['errors'][] = "blank course code";
-        }
-        if(empty($sectionnum)) {
-            $_SESSION['errors'][] = "blank section number";
-        }
-        if(empty($edollar)) {
-            $_SESSION['errors'][] = "blank e-dollar";
+    // Check for empty fields
+    if (empty($courseCode) || empty($sectionNum) || empty($edollar)) {
+        if (empty($courseCode)) {
+            $_SESSION['errors'][] = "Course code cannot be blank";
+        } if (empty($sectionNum)) {
+            $_SESSION['errors'][] = "Section number cannot be blank";
+        } if (empty($edollar)) {
+            $_SESSION['errors'][] = "E-dollar cannot be blank";
         }
         header("Location: placebid.php");
         exit();
     }
+
+    // Initialise DAOs for validations
+    $courseDAO = new CourseDAO();
+    $sectionDAO = new SectionDAO();
+
+    // Check for valid course code
+    if(!($courseDAO->retrieve($courseCode))) {
+        $_SESSION['errors'][] = "Invalid course code";
+    } else {
+        if (!($sectionDAO->retrieve($courseCode, $sectionNum))) {
+            // Check for valid section (only if course code is valid)
+            $_SESSION['errors'][] = "Invalid section";
+        }
+    }
+
+    // Check for valid e-dollar
+
+    if (!(is_numeric($edollar) || is_float($edollar))) {
+        $_SESSION['errors'][] = "Please enter a valid number for E-dollar";
+    } else {
+        $checkedEdollar = strval($edollar);
+        $edollarArr = explode(".", $checkedEdollar);
+        if(strlen($edollarArr[1]) > 2){
+            $_SESSION['errors'][] = "E-dollar can only have up to 2 decimal places";
+        }
+    }
+
+    // If inputs do not pass field and data validations, redirect user back to placebid.php immediately
+    if (count($_SESSION['errors'])) {
+        header("Location: placebid.php");
+    }
+
+    // Initialise the rest of the DAOs and objects needed if data validations are passed
+    $prerequisiteDAO = new PrerequisiteDAO();
+    $studentDAO = new StudentDAO();
+    $bidDAO = new BidDAO();
+    $roundDAO = new RoundDAO();
+    $currentRound = $roundDAO->retrieve()->getRound();
+    $student = $studentDAO->retrieve($userid);
+    $studentBids = $bidDAO->retrieveByUserid($userid);
+    $course = $courseDAO->retrieve($courseCode);
+    $section = $sectionDAO->retrieve($courseCode, $sectionNum);
+
+    // Perform logic validations only if input data validations are passed
+
+    // Check if bid is above min bid
+    if (($currentRound == 1 && $edollar < 10.0) || ($currentRound == 2 && $edollar < $minBid)) {
+        $_SESSION['errors'][] = "Your bid is less than the minimum bid";
+    }
+
+    // Check if student has enough e-dollars
+    if ($edollar > $student->getEdollar()) { // Check if student has enough e-dollars
+        $_SESSION['errors'][] = "You do not have enough e-dollars to make this bid";
+    }
+
+    // Check for class timetable clash
+
+    // Check for exam timetable clash
+
+    // Check if course has a prerequisite, and if the student has completed it
+    if ($prerequisiteDAO->retrieve($courseCode)) {
+        $prerequisite = $prerequisiteDAO->retrieve($courseCode);
+        if(!($courseCompletedDAO->retrieveByUserIdAndCode($userid, $prerequisite->getPrerequisite()))){
+            $_SESSION['errors'][] = "You have not yet completed the prerequisite for this course";
+        }
+    }
+
+    // Check if student has already completed the course
+
+    // Check if student is already enrolled in the course
+    
+    // Check if student already made 5 bids
+    if (count($studentBids) == 5) {
+        $_SESSION['errors'][] = "You already reached your maximum number of bids";
+    }
+
+    // Check if student has already bidded for this course
+    $alreadyBidded = FALSE;
+    foreach($studentBids as $b) {
+        if ($b->getCode() == $courseCode) {
+            $alreadyBidded == TRUE;
+        }
+    }
+    if ($alreadyBidded) {
+        $_SESSION['errors'][] = "You have already bidded for another section in this course";
+    }
+
+    // If round 1, check if course is offered by student's school
+    if ($currentRound == 1 && !($student->getSchool() == $course->getSchool())) {
+        $_SESSION['errors'][] = "You can only bid for courses offered by your school in Bidding Round 1";
+    }
+
+    // If round 2, check if there are vacancies in the section
 ?>
