51c7b4718f81380537ded6869bce4797edd5e360
Steffi <steffi.tan.2018@economics.smu.edu.sg>
2019-10-14 12:10:11 +0800

    updated boostrap_process and admin bootstrap form to process errors
    Co-Authored-By: j1em1n <j1em1n@users.noreply.github.com>

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,9 +3,23 @@ require_once 'include/protect.php';
 require_once 'include/common.php';
 
 ?>
-
-<form id='bootstrap-form' action="bootstrap_process.php" method="post" enctype="multipart/form-data">
-	Bootstrap file: 
-	<input id='bootstrap-file' type="file" name="bootstrap-file"></br>
-	<input type="submit" name="submit" value="Import">
-</form>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="include/style.css">
+	</head>
+	<body>
+		<form id='bootstrap-form' action="bootstrap_process.php" method="post" enctype="multipart/form-data">
+			Bootstrap file: 
+			<input id='bootstrap-file' type="file" name="bootstrap-file"></br>
+			<input type="submit" name="submit" value="Import">
+		</form>
+		<?php
+			printErrors();
+			if(isset($_SESSION['success'])) {
+				echo "<h3>{$_SESSION['success']}</h3>";
+				unset($_SESSION['success']);
+			}
+		?>
+	</body>
+</html>
--- app/bootstrap_process.php
+++ app/bootstrap_process.php
@@ -1,4 +1,11 @@
 <?php
 # edit the file included below. the bootstrap logic is there
 require_once 'include/bootstrap.php';
-doBootstrap();
+$errors = doBootstrap();
+if(!empty($errors)) {
+    $_SESSION['errors'] = $errors;
+    header("Location: bootstrap.php");
+} else {
+    $_SESSION['success'] = "Bootstrap successful! Bidding Round 1 started.";
+    header("Location: boostrap.php");
+}
