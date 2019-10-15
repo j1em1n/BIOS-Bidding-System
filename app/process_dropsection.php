6f4fe8fb045e110c43ff878f14df8cc77dd8fab1
hilyamy2018 <54932191+hilyamy2018@users.noreply.github.com>
2019-10-15 12:42:08 +0800

    Update process_dropsection.php
    To get coursecode + sectionnum + biddedamount in order to update user's eDollars.

--- app/process_dropsection.php
+++ app/process_dropsection.php
@@ -6,80 +6,85 @@
     $sectionnum = $_POST['sectionnum'];
     $userid = $_SESSION['userid'];
 
+    // check if round is active
+    $roundDAO = new RoundDAO();
+    $roundinfo = $roundDAO->retrieveRoundInfo();
+    $roundnum = $roundinfo->getRoundNum();
+    $roundstatus = $roundinfo->getStatus();
+    var_dump($roundstatus);
 
-    //if everything is added in successfully
-    
-    if (!empty($coursecode) && !empty($sectionnum)){
 
-        $courseDAO = new CourseDAO();
-        $sectionDAO = new SectionDAO();
-        $bidDAO = new BidDAO();
-        $studentDAO = new StudentDAO();
+    //if round is active:
+    if($roundstatus == 'opened' ){ // rmb to include round num before push
 
-        // Check if course exists in database
-        if(!($courseDAO->retrieve($coursecode))) {
-            $_SESSION['errors'][] = "invalid course code";
+        // check if user has entered all fields
+        if (!empty($coursecode) && !empty($sectionnum)){
 
-            header("Location: dropbid.php");
-            exit();
+            $courseDAO = new CourseDAO();
+            $sectionDAO = new SectionDAO();
+            $bidDAO = new BidDAO();
+            $studentDAO = new StudentDAO();
+
+            // Check if course exists in database
+            if(!($courseDAO->retrieve($coursecode))) {
+                $_SESSION['errors'][] = "invalid course code";
+
+                header("Location: dropsection.php");
+                exit();
+            
+            
+            } else { //course exists
+                if (!($sectionDAO->getSectionsByCourse($coursecode))) {
+                    // Check if section code is found in section.csv (only for valid course code)
+                    $_SESSION['errors'][] = "invalid section";
+
+                    header("Location: dropsection.php");
+                    exit();
+
+                    // section in course
+                } else { // course and section exist
+
+                    // now check against USER's BID INFO
+                    $student = $studentDAO->retrieve($userid);
+                    $currentedollars = $student->getEdollar();
+                    $updatedamount = 0.0;
+                    // get all bids from this user 
+                    $listofbids = $bidDAO->retrieveByUserid($userid);
+                    var_dump($listofbids);
+
+                    foreach($listofbids as $eachbid){
+                        if($eachbid->getCode() == $coursecode && $eachbid->getSection() == $sectionnum ){
+                            $bidamount = $eachbid->getAmount();
+                            var_dump($bidamount);
+
+                            //drop section
+                            
+
+                            //update E dollars
+                            $updatedamount = $currentedollars + $bidamount;
+                            var_dump($updatedamount);
+                        
+                        }
+                    }
+                }
+            } 
 
         } else {
-            if (!($sectionDAO->getSectionsByCourse($coursecode))) {
-                // Check if section code is found in section.csv (only for valid course code)
-                $_SESSION['errors'][] = "invalid section";
+            if(empty($coursecode)) {
+                $_SESSION['errors'][] = "blank course code";
+            }
+            if(empty($sectionnum)) {
+                $_SESSION['errors'][] = "blank section number";
+            }
 
-                header("Location: dropbid.php");
-                exit();
-
-            } else {
-
-                $student = $studentDAO->retrieve($userid);
-                $currentedollars = $student->edollar;
-                $updatedamount = 0.0;
-                // get all bids from this user 
-                $listofbids = $bidDAO->retrieveByUserid($userid);
-                //var_dump($listofbids);
-                var_dump($currentedollars);
-
-                //specifiy which bid want to drop based on userid and coursecode and sectionnum
-                //foreach($listofbids as $eachbid){
-                    if($listofbids->userid == $userid && $listofbids->code == $coursecode 
-                        && $listofbids->section == $sectionnum){
-                        // update edollars by subtraction
-                        $biddedamount = $listofbids->amount;                        
-                        $updatedamount =  strval($currentedollars - $biddedamount);
-
-                        var_dump($updatedamount);
-                        var_dump($userid);
-
-                        // update in database
-                        $studentDAO->updateEdollar($userid, $updatedamount) ;
-                        
-                        header("Location: dropbid.php");
-                        exit();
-                    }
-
-                
-
-               
-            } 
+            header("Location: dropsection.php");
+            exit();
         }
-        
-
-
-
-
-
-
     } else {
-        if(empty($coursecode)) {
-            $_SESSION['errors'][] = "blank course code";
-        }
-        if(empty($sectionnum)) {
-            $_SESSION['errors'][] = "blank section number";
-        }
+            $_SESSION['errors'][] = "There is currently no active round";
 
-        header("Location: dropbid.php");
-        exit();
+            header("Location: dropsection.php");
+            exit();
+        
     }
 ?>
