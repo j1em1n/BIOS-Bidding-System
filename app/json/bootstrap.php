0caebb610906a771a0b6c5e03607301d329c1508
hilyamy2018 <54932191+hilyamy2018@users.noreply.github.com>
2019-10-30 13:40:05 +0800

    Created bootstrap_form.php and updated bootstrap.php for JSON
    Co-Authored-By: slku1 <slku1@users.noreply.github.com>

--- app/json/bootstrap.php
+++ app/json/bootstrap.php
@@ -1,4 +1,17 @@
 <?php
-require_once '../include/bootstrap.php';
+    require_once '../include/common.php';
+    require_once '../include/bootstrap.php';
+    require_once '../include/token.php';
 
-# complete bootstrap
+    $errors = commonValidationsJSON(basename(__FILE__));
+    $success = array();
+
+    if (!empty($errors)) {
+        $result = jsonErrors($errors);
+    } else {
+        $result = bootstrapJSON();
+    }
+    
+    header('Content-Type: application/json');
+	echo json_encode($result, JSON_PRETTY_PRINT);
+?>
---
+++ app/json/bootstrap_form.php
@@ -0,0 +1,7 @@
+<form action="http://localhost/github/spm-unofficial/app/json/bootstrap"  method="post" enctype="multipart/form-data">
+  File:
+  <input type="file" name="bootstrap-file" /><br />
+  <input type='text' name='token' value='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiZGF0ZXRpbWUiOiIyMDE5LTEwLTI5IDA5OjM5OjU2In0.cgZ1NRNR96-0vser8uC5kvXpjCxnha1TjzSfuchtU78' />
+  <!-- substitute the above value with a valid token -->
+  <input type="submit" value="Bootstrap" />
+</form>
