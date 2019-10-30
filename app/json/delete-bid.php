ed0f6d05b39be694ad47eff5a94a31892a72e000
hilyamy2018 <54932191+hilyamy2018@users.noreply.github.com>
2019-10-30 14:10:12 +0800

    Updated delete-bid.php
    Co-Authored-By: slku1 <slku1@users.noreply.github.com>

--- app/json/delete-bid.php
+++ app/json/delete-bid.php
@@ -1,20 +1,19 @@
 <?php
-
-    require_once '../include/protect.php';
-    //require_once '../include/protect_roundclosed.php';
     require_once '../include/common.php';
+    require_once '../include/token.php';
 
-    // isMissingOrEmpty(...) is in common.php
-    $errors = [ isMissingOrEmpty ('userid'), 
-                isMissingOrEmpty ('course'),
-                isMissingOrEmpty ('section') ];
-    $errors = array_filter($errors);
+    $errors = commonValidationsJSON(basename(__FILE__));
+    $success = array();
 
-    // if there is no blank/empty field
-    if (isEmpty($errors)) {
-        $userid = $_POST['userid'];
-        $courseCode = $_POST['course'];
-        $sectionNum = $_POST['section'];
+    if (!empty($errors)) {
+        $result = jsonErrors($errors);
+    } else {
+        // Retrieve 'r' GET parameter
+        $jsonObj = json_decode($_REQUEST['r']);
+    
+        $userid = $jsonObj->userid;
+        $courseCode = $jsonObj->course;
+        $sectionNum = $jsonObj->section;
     
         // Initialise DAOs and objects needed for validations
         $courseDAO = new CourseDAO();
@@ -26,54 +25,55 @@
 
         // Check for valid course code
         if (!($courseDAO->retrieve($courseCode))) {
-            $errors = ["invalid course"];
+            $errors[] = "invalid course";
         } else {
             if (!($sectionDAO->retrieve($courseCode, $sectionNum))) {
                 // Check for valid section (only if course code is valid)
-                $errors = ["invalid section"];
+                $errors[] = "invalid section";
             }
         }
 
         // Check if userid is valid
         if (!($studentDAO->retrieve($userid))){
-            $errors = ["invalid userid"];
+            $errors[] = "invalid userid";
         }
 
         // Check for any active round
         if ($currentStatus == "closed"){
-            $errors = ["round ended"];
+            $errors[] = "round ended";
         }
 
-        // If there is active bidding round, (course, userid and section are valid) and round is currently active
-        if (isEmpty($errors)){
-            if (!($bidDAO->retrieve($userid, $courseCode, $sectionNum))){
-                $errors = ["no such bid"];
-            } else {
-                $currentedollars = $student->getEdollar();
-                $updatedamount = 0.0;
-
-                $selected_bid = $bidDAO->retrieve($userid, $courseCode, $sectionNum);
-                    
-                $updatedamount =  strval($currentedollars + $biddedamount);
-                $isDeleteOK = $bidDAO->delete($selected_bid);
-
-                if ($isDeleteOK) {
-                    $studentDAO->updateEdollar($userid, $updatedamount);
-                    $result = [
-                        "status" => "success" 
-                    ];
-                }
+        // Check if bid exists only if course, userid and section are valid and round is currently active
+        if (empty($errors)) {
+            $bid = $bidDAO->retrieve($userid, $courseCode);
+            if (!($bid) || $bid->getStatus() != "Pending"){
+                $errors[] = "no such bid";
             }
         }
-        
-    }
 
-    if(!(isEmpty($errors))){
-        $final_errors = array_multisort($errors);
-        $result = [
-            "status" => "error",
-            "messages" => array_values($final_errors)
-        ];    
+        if (empty($errors)){
+            $student = $studentDAO->retrieve($userid);
+            $currentedollars = $student->getEdollar();
+            $selected_bid = $bidDAO->retrieve($userid, $courseCode);
+            $biddedamount = $selected_bid->getAmount();
+            $updatedamount =  $currentedollars + $biddedamount;
+            $isDeleteOK = $bidDAO->delete($selected_bid);
+
+            if ($isDeleteOK) {
+                $studentDAO->updateEdollar($userid, $updatedamount);
+                $success = [
+                    "status" => "success" 
+                ];
+            } else {
+                $errors[] = "error: could not delete bid";
+            }
+        }
+        if (empty($errors) && !empty($success)) {
+            $result = $success;
+        } else {
+            sort($errors);
+            $result = jsonErrors($errors);
+        }
     }
 
     header('Content-Type: application/json');
