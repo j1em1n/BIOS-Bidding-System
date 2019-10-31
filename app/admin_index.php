86376f4d984b35d52ab32740053a583b99e45f62
Steffi <steffi.tan.2018@economics.smu.edu.sg>
2019-10-31 18:06:09 +0800

    updated admin index UI
    Co-Authored-By: slku1 <slku1@users.noreply.github.com>

--- app/admin_index.php
+++ app/admin_index.php
@@ -1,13 +1,28 @@
 <?php
 require_once 'include/protect.php';
 require_once 'include/common.php';
+
+$roundDAO = new RoundDAO();
+$roundInfo = $roundDAO->retrieveRoundInfo();
+$currentRound = $roundInfo->getRoundNum();
+$currentStatus = $roundInfo->getStatus();
+
 ?>
 
 <html>
-    <h1>Welcome, Admin</h1>
+    <head>
 
-    <p><a href='bootstrap.php'>Bootstrap</a></p>
-    <p><a href='adminround.php'>Open / Close Bidding Round</a></p>
-    <p><a href='logout.php'>Logout</a></p>
+            <div style='float: right;'><a href='logout.php'>Logout</a></div>
+        <link rel="stylesheet" type="text/css" href="include/style.css">
+    </head>
+    <body>
+        <h1>BIOS BIDDING (Administrator)</h1>
+	<h2>Welcome, Admin</h2>
+	<h3>Current Round: <?=$currentRound?> (<?=strtoupper($currentStatus)?>)</h3>
+
+        <p><a href='bootstrap.php'>Bootstrap</a></p>
+        <p><a href='adminround.php'>Open / Close Bidding Round</a></p>
+        <p><a href='logout.php'>Logout</a></p>
+    </body>
 </html>
 
