3e3603e76ab1aa5a2886695b5efb1355b93c7c62
hilyamy2018 <54932191+hilyamy2018@users.noreply.github.com>
2019-11-04 18:11:42 +0800

    Updated bootstrap.php
    For UI

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -10,13 +10,17 @@ require_once 'include/common.php';
 	</head>
 	<body>
 		<form id='bootstrap-form' action="bootstrap_process.php" method="post" enctype="multipart/form-data">
-			Bootstrap file: 
-			<input id='bootstrap-file' type="file" name="bootstrap-file"></br>
-			<input type="submit" name="submit" value="Import">
+			<table>
+				<tr>
+					<td>Bootstrap file: </td>
+					<td><input id='bootstrap-file' type="file" name="bootstrap-file"></td>
+					<td><input type="submit" name="submit" value="Import"></td>
+				</tr>
+			</table>	
 		</form>
-		<p>
-			<a href="admin_index.php">Home</a>
-		</p>
+				<p>
+					<input class = "button1" type = "button" value = "Home" onclick = "window.location.href='admin_index.php'"><br>
+				</p>
 		<?php
 			printSuccess();
 			printErrors();
