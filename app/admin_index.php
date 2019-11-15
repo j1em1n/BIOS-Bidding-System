89bad76f4f0fd3606c29e354657368a1558cf1c7
hilyamy2018 <54932191+hilyamy2018@users.noreply.github.com>
2019-11-15 17:17:34 +0800

    update admin_index.php

--- app/admin_index.php
+++ app/admin_index.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'include/protect.php';
 require_once 'include/common.php';
-require_once 'include/navbar.php';
+require_once 'include/navbar_admin.php';
 
 $roundDAO = new RoundDAO();
 $roundInfo = $roundDAO->retrieveRoundInfo();
@@ -18,7 +18,16 @@ $currentStatus = $roundInfo->getStatus();
     <body>
 
     <h1>BIOS BIDDING (Administrator)</h1>
-	<h3>Current Round: <?=$currentRound?> <b>(<?=strtoupper($currentStatus)?>)<b></h3>
+    <h3>Current Round: <?=$currentRound?> 
+
+    <?php
+    if($currentStatus == 'closed'){
+                echo "<span style = 'color:red'><b>(".strtoupper($currentStatus).")</b>";
+            } else {
+                echo "<span style = 'color:green'><b>(".strtoupper($currentStatus).")</b>";
+            }
+
+    ?><br><br>
     
     <input class = "button2" type = "button" value = "Bootstrap" onclick = "window.location.href='bootstrap.php'"/><br>
     <br>
